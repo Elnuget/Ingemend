1e0ea6b5a027de362abc7ef3c3da72661547135d
Elnuget <100396747+Elnuget@users.noreply.github.com>
2023-02-08 12:28:40 -0500

    Init commit

---
+++ app/src/main/java/com/ryc/ingemend/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ryc.ingemend;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
